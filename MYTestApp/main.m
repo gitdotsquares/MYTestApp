396f1e6201d3a375d3d6af28f0fca144b455488b
gitdotsquares <sudhanshu@dotsquares.com>
2016-03-16 16:35:37 +0530

    Commit
    Commit

---
+++ MYTestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MYTestApp
+//
+//  Created by Sumit Shastri on 3/16/16.
+//  Copyright © 2016 ds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
